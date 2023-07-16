97e7dfecd258c0e2d46f743429a3be937217dbe1
Joen Tan <joentze01@gmail.com>
2023-07-16 15:28:10 +0800

    feat: added tiktok video downloader

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from auth.browser_login import access_login_page
 from details.login_configs import twitter_details, tiktok_details, reddit_details
 
 if __name__ == "__main__":
-    access_login_page("https://twitter.com/login", twitter_details)
+    access_login_page("https://tiktok.com", tiktok_details)
--- rest_helpers/tiktok_handler.py
+++ rest_helpers/tiktok_handler.py
@@ -1,8 +1,12 @@
 """handles tiktok searches"""
 import json
+import random
 from typing import List, TypedDict
 from urllib.parse import quote
-from requests import get, Response
+import urllib.request
+from requests import get, Response, ConnectionError
+
+TIKTOK_REFERER = "https://www.tiktok.com/"
 
 
 class TikTokDownloadRequest(TypedDict):
@@ -41,6 +45,29 @@ def get_tiktok_search(search_term: str, no_of_pages: int) -> List[object]:
     return all_entries
 
 
+def download_tiktok_video(url: str) -> None:
+    """downloads tiktok video from url"""
+    try:
+        tiktok_video_header: TikTokDownloadRequest = {
+            "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "cookie": TIKTOK_SECRETS["cookie"],
+            "referer": TIKTOK_REFERER
+        }
+        random_hash = random.getrandbits(128)
+        opener = urllib.request.build_opener()
+        headers = [(header_name, header_content)
+                   for header_name, header_content in tiktok_video_header.items()]
+        print(headers)
+        opener.addheaders = headers
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(url=url, filename=f"{random_hash}.mp4")
+
+    except ConnectionError:
+        pass
+
+
 if __name__ == "__main__":
-    tiktoks = get_tiktok_search("#ukraine", 5)
-    print(len(tiktoks))
+    # tiktoks = get_tiktok_search("#ukraine", 5)
+    # print(len(tiktoks))
+    # download_tiktok_video("https://v19-webapp-prime.tiktok.com/video/tos/alisg/tos-alisg-pve-0037c001/o8mIoELrwAb8AVHxIftLpyzANDUNohCymD7wQl/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=3364&bt=1682&cs=0&ds=3&ft=3.u4FZPw0PD12M~Q2t3wUGx15SHEg9N1O-lc&mime_type=video_mp4&qs=0&rc=aDlmZTo1Z2VoODpnOGU1NkBpM2tpbDo6ZnJsbDMzODczNEAxMzQuMWMwNTMxLTQxLzVeYSNoMnNecjRfb2ZgLS1kMS1zcw%3D%3D&btag=e00080000&expire=1689513790&l=20230716072253E91FE0A109CEF9ADA420&ply_type=2&policy=2&signature=5e33c5bb05c54d8b794cce73ef8a66ca&tk=tt_chain_token")
+    pass
